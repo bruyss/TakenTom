92b1e0e4c0150b4e124b2da1867db42553eae848
Jeroen Van Bruyssel <jeroen.vb1@gmail.com>
2022-11-19 17:15:34 +0100

    Antwoord taak 2

--- examen_taak2.py
+++ examen_taak2.py
@@ -1,7 +1,36 @@
 #! python3
 
+def voorwaartse_reeks(*args: str) -> str:
+    return "".join(args).lower()
+
+
+def achterwaartse_reeks(*args: str) -> str:
+    return "".join(args).lower()[::-1]
+
+
+def gemeenschappelijke_reeks(*args: str) -> str:
+    voorwaarts = voorwaartse_reeks(*args)
+    achterwaarts = achterwaartse_reeks(*args)
+    for i in range(len(voorwaarts)):
+        # print(voorwaarts[i:])
+        # print(achterwaarts[:-i])
+        if voorwaarts[i:] == achterwaarts[:-i]:
+            return voorwaarts[i:] 
+    return "" 
+
+
+def ontbrekend_woord(*args: str) -> str:
+    achterwaarts = achterwaartse_reeks(*args)
+    gemeen = gemeenschappelijke_reeks(*args)
+    return achterwaarts.lstrip(gemeen).capitalize()
+
+
 def main():
-    pass
+    print(voorwaartse_reeks("Knalpot", "Tegengas", "Allebei", "Wegrenner", "Gewiebel", "Lasagne", "Getto"))
+    print(achterwaartse_reeks("Knalpot", "Tegengas", "Allebei", "Wegrenner", "Gewiebel", "Lasagne", "Getto"))
+    print(gemeenschappelijke_reeks("Knalpot", "Tegengas", "Allebei", "Wegrenner", "Gewiebel", "Lasagne", "Getto"))
+    print(ontbrekend_woord("Knalpot", "Tegengas", "Allebei", "Wegrenner", "Gewiebel", "Lasagne", "Getto"))
+
 
 if __name__ == "__main__":
     main()
